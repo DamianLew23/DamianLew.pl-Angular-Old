d16569e73ea6972093af2c5c15054cb1ba0e5890
Damian Lew <damian@MacBook-Pro.local>
2020-07-23 14:20:45 +0200

    create structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {CoreModule} from './core/core.module';
+import {HomeModule} from './modules/home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
