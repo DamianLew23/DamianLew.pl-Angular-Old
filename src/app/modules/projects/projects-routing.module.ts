fc3fc4c61cb6de386755fbdd5b7e478fdfbe495d
Damian Lew <damian@MacBook-Pro.local>
2020-08-05 11:42:27 +0200

    portfolio angular ready

--- src/app/modules/projects/projects-routing.module.ts
+++ src/app/modules/projects/projects-routing.module.ts
@@ -6,8 +6,23 @@ import {PaskoAutoserwisComponent} from './pages/wordpress/pasko-autoserwis/pasko
 import {LgfalownikiComponent} from './pages/wordpress/lgfalowniki/lgfalowniki.component';
 import {EpocztaonlineComponent} from './pages/wordpress/epocztaonline/epocztaonline.component';
 import {PortaltechnologiiComponent} from './pages/wordpress/portaltechnologii/portaltechnologii.component';
+import {MyportfolioComponent} from './pages/angular/myportfolio/myportfolio.component';
+import {DobierzfalownikFrontendComponent} from './pages/angular/dobierzfalownik-frontend/dobierzfalownik-frontend.component';
+import {PoradyzakupoweFrontendComponent} from './pages/angular/poradyzakupowe-frontend/poradyzakupowe-frontend.component';
 
 const projectsRoutes: Routes = [
+  {
+    path: 'poradyzakupowe-angular',
+    component: PoradyzakupoweFrontendComponent
+  },
+  {
+    path: 'dobierzfalownik-angular',
+    component: DobierzfalownikFrontendComponent
+  },
+  {
+    path: 'damianlew-angular',
+    component: MyportfolioComponent
+  },
   {
     path: 'poradyzakupowe-wordpress',
     component: PoradyzakupoweComponent
